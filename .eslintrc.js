2ffb8a437fd99b74909279cc2d6734a77544fd02
DAO1 Team <developer@dao1.org>
2021-06-30 11:51:14 +0300

    Set react/prop-types to 0 DAO-22

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
- 
 module.exports = {
-    env: {
-      browser: true,
-      commonjs: true,
-      es2021: true,
-      jest: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['plugin:react/recommended', 'standard', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: ['plugin:react/recommended', 'standard', 'prettier'],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-    },
-    plugins: ['react'],
-    rules: {
-      'no-unused-vars': 1,
-    },
-  }
+    ecmaVersion: 12,
+  },
+  plugins: ['react'],
+  rules: {
+    'no-unused-vars': 1,
+    'react/prop-types': 0,
+  },
+}
