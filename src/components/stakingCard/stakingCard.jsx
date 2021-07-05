e704fe64ab5fca055d3ff29a7501634faee93e91
DAO1 Team <developer@dao1.org>
2021-07-05 18:02:28 +0300

    Add styles for stakingCard component DAO-31

--- src/components/stakingCard/stakingCard.jsx
+++ src/components/stakingCard/stakingCard.jsx
@@ -1,25 +1,227 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classnames from 'classnames'
 
+import DAO1Logo from '../../assets/white-logo.png'
+import ArrowIcon from '../../assets/arrow-down.png'
 import {
-  Card,
-  CardHead,
-  CardStakingConditions,
-  CardStakingList,
-  CardFooter,
+  card,
+  cardHead,
+  cardHeadLogo,
+  cardLabel,
+  cardInfoText,
+  cardName,
+  cardNameText,
+  cardStakingConditions,
+  cardStakingConditionsItem,
+  cardStakingList,
+  cardStakingItem,
+  cardStakingItemHead,
+  cardStakingItemInfo,
+  cardStakingItemInfoBlock,
+  cardStakingItemButtons,
+  cardStakingItemDetails,
+  cardStakingItemDetailsHide,
+  cardStakingItemDetailsRow,
+  cardStakingItemDetailsName,
+  cardStakingItemDetailsValue,
+  cardArrowButton,
+  cardArrowButtonActive,
+  cardFooter,
+  cardTatalStaked,
+  cardTatalStakedValue,
+  cardButton,
 } from './stakingCard.module.scss'
 import { Button } from '../'
 
-export function StakingCard({ title, daysAmount, yieldPercent }) {
+export function StakingCard({
+  name,
+  daysAmount,
+  yieldPercent,
+  stakingHistory,
+}) {
+  const [visibleDetailedBlock, setVisibleDetailedBlock] = useState(null)
+  const accordionClickHandler = (id) => {
+    if (id === visibleDetailedBlock) {
+      setVisibleDetailedBlock(null)
+      return
+    }
+
+    setVisibleDetailedBlock(id)
+  }
+
+  const StakingHistory = () =>
+    stakingHistory.map(({ staked, claimed, expires, details }, idx) => {
+      const isActive = visibleDetailedBlock === idx
+      const hiddenClassNames = classnames(
+        cardStakingItemDetails,
+        cardStakingItemDetailsHide,
+      )
+      const detailsClassNames = isActive
+        ? cardStakingItemDetails
+        : hiddenClassNames
+      const activeArrowClassNames = classnames(
+        cardArrowButton,
+        cardArrowButtonActive,
+      )
+      const arrowButtonClassnames = isActive
+        ? activeArrowClassNames
+        : cardArrowButton
+
+      return (
+        <div key={idx} className={cardStakingItem}>
+          <div className={cardStakingItemHead}>
+            <div className={cardStakingItemInfo}>
+              <div className={cardStakingItemInfoBlock}>
+                <div className={cardLabel}>DAO1 Staked</div>
+                <div className={cardInfoText}>{staked}</div>
+              </div>
+              <div className={cardStakingItemInfoBlock}>
+                <div className={cardLabel}>Claimable</div>
+                <div className={cardInfoText}>{claimed}</div>
+              </div>
+              <div className={cardStakingItemInfoBlock}>
+                <div className={cardLabel}>Expires</div>
+                <div className={cardInfoText}>{expires}</div>
+              </div>
+            </div>
+
+            <div className={cardStakingItemButtons}>
+              <Button className={cardButton}>Claimed</Button>
+              <Button className={cardButton} disabled>
+                Unstaked
+              </Button>
+              <button
+                onClick={() => accordionClickHandler(idx)}
+                className={arrowButtonClassnames}
+              >
+                <img src={ArrowIcon} alt="Arrow" />
+              </button>
+            </div>
+          </div>
+
+          <div className={detailsClassNames}>
+            <div className={cardStakingItemDetailsRow}>
+              <div className={cardStakingItemDetailsName}>Stake Identifier</div>
+              <div className={cardStakingItemDetailsValue}>
+                {details.identifier}
+              </div>
+            </div>
+            <div className={cardStakingItemDetailsRow}>
+              <div className={cardStakingItemDetailsName}>Stake status</div>
+              <div className={cardStakingItemDetailsValue}>
+                {details.status}
+              </div>
+            </div>
+            <div className={cardStakingItemDetailsRow}>
+              <div className={cardStakingItemDetailsName}>
+                Start date (stake placement)
+              </div>
+              <div className={cardStakingItemDetailsValue}>{details.start}</div>
+            </div>
+            <div className={cardStakingItemDetailsRow}>
+              <div className={cardStakingItemDetailsName}>
+                End date (stake expiration)
+              </div>
+              <div className={cardStakingItemDetailsValue}>{details.end}</div>
+            </div>
+            <div className={cardStakingItemDetailsRow}>
+              <div className={cardStakingItemDetailsName}>Staked amount</div>
+              <div className={cardStakingItemDetailsValue}>
+                {details.amount} DAO1
+              </div>
+            </div>
+            <div className={cardStakingItemDetailsRow}>
+              <div className={cardStakingItemDetailsName}>
+                Fee for early (before expire) unstake
+              </div>
+              <div className={cardStakingItemDetailsValue}>
+                {details.feeForEarlyUnstake}
+              </div>
+            </div>
+            <div className={cardStakingItemDetailsRow}>
+              <div className={cardStakingItemDetailsName}>
+                Total yield (for entire period)
+              </div>
+              <div className={cardStakingItemDetailsValue}>
+                {details.totalYield}
+              </div>
+            </div>
+            <div className={cardStakingItemDetailsRow}>
+              <div className={cardStakingItemDetailsName}>
+                Locked yield (releases over time)
+              </div>
+              <div className={cardStakingItemDetailsValue}>
+                {details.lockedYield} DAO1
+              </div>
+            </div>
+            <div className={cardStakingItemDetailsRow}>
+              <div className={cardStakingItemDetailsName}>
+                Released yield (claimed + claimable)
+              </div>
+              <div className={cardStakingItemDetailsValue}>
+                {details.releasedYield} DAO1
+              </div>
+            </div>
+            <div className={cardStakingItemDetailsRow}>
+              <div className={cardStakingItemDetailsName}>Claimed yield</div>
+              <div className={cardStakingItemDetailsValue}>
+                {details.claimedYield} DAO1
+              </div>
+            </div>
+            <div className={cardStakingItemDetailsRow}>
+              <div className={cardStakingItemDetailsName}>
+                Claimable now (available for withdrawal)
+              </div>
+              <div className={cardStakingItemDetailsValue}>
+                {details.claimedNow} DAO1
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+    })
+
   return (
-    <div className={Card}>
-      <div className={CardHead}>{title}</div>
-      <div className={CardStakingConditions}>
-        <div>{daysAmount}</div>
-        <div>{yieldPercent}</div>
+    <div className={card}>
+      <div className={cardHead}>
+        <div className={cardHeadLogo}>
+          <img src={DAO1Logo} alt="DAO1" />
+        </div>
+        <div className={cardName}>
+          <span className={cardLabel}>Name</span>
+          <span className={cardNameText}>{name}</span>
+        </div>
       </div>
-      <div className={CardStakingList}>Staking List</div>
-      <div className={CardFooter}>
-          <Button>Stake</Button>
+
+      <div className={cardStakingConditions}>
+        <div className={cardStakingConditionsItem}>
+          <div className={cardLabel}>{daysAmount}D yield</div>
+          <div className={cardInfoText}>{yieldPercent}%</div>
+        </div>
+        <div className={cardStakingConditionsItem}>
+          <div className={cardLabel}>Daily yield</div>
+          <div className={cardInfoText}>0.0516%</div>
+        </div>
+        <div className={cardStakingConditionsItem}>
+          <div className={cardLabel}>APY-{daysAmount}D compound</div>
+          <div className={cardInfoText}>20.2%</div>
+        </div>
+        <div className={cardStakingConditionsItem}>
+          <div className={cardLabel}>Duration</div>
+          <div className={cardInfoText}>{daysAmount} Days</div>
+        </div>
+      </div>
+
+      <div className={cardStakingList}>
+        <StakingHistory />
+      </div>
+
+      <div className={cardFooter}>
+        <div className={cardTatalStaked}>
+          <div className={cardLabel}>Total Staked:</div>
+          <div className={cardTatalStakedValue}>246.246 DAO1</div>
+        </div>
+        <Button className={cardButton}>Stake</Button>
       </div>
     </div>
   )
--- src/pages/staking/staking.jsx
+++ src/pages/staking/staking.jsx
@@ -1,7 +1,73 @@
 import React from 'react'
 
-import { MainLayout } from '../../components'
+import { MainLayout, StakingCard } from '../../components'
+
+const datiledStakingFakeData = [
+  {
+    staked: '0.0',
+    claimed: '0.0',
+    expires: 'Expired',
+    details: {
+      identifier: 2,
+      status: 'Active',
+      start: '10 Jun 2021 00:12 UTC',
+      end: '10 Jul 2021 00:12 UTC',
+      amount: 123.123,
+      feeForEarlyUnstake: '1.55% or 1.908 DAO1',
+      totalYield: '1.55% or 1.908 DAO1',
+      lockedYield: 1.234,
+      releasedYield: 0.674,
+      claimedYield: 0.673,
+      claimedNow: 0.001,
+    },
+  },
+  {
+    staked: 0.0,
+    claimed: 0.0,
+    expires: 'Expired',
+    details: {
+      identifier: 2,
+      status: 'Active',
+      start: '10 Jun 2021 00:12 UTC',
+      end: '10 Jul 2021 00:12 UTC',
+      amount: 123.123,
+      feeForEarlyUnstake: '1.55% or 1.908 DAO1',
+      totalYield: '1.55% or 1.908 DAO1',
+      lockedYield: 1.234,
+      releasedYield: 0.674,
+      claimedYield: 0.673,
+      claimedNow: 0.001,
+    },
+  },
+  {
+    staked: 0.0,
+    claimed: 0.0,
+    expires: 'Expired',
+    details: {
+      identifier: 2,
+      status: 'Active',
+      start: '10 Jun 2021 00:12 UTC',
+      end: '10 Jul 2021 00:12 UTC',
+      amount: 123.123,
+      feeForEarlyUnstake: '1.55% or 1.908 DAO1',
+      totalYield: '1.55% or 1.908 DAO1',
+      lockedYield: 1.234,
+      releasedYield: 0.674,
+      claimedYield: 0.673,
+      claimedNow: 0.001,
+    },
+  },
+]
 
 export const Staking = () => {
-  return <MainLayout>Content</MainLayout>
+  return (
+    <MainLayout>
+      <StakingCard
+        name="DAO1 — FIXED 30 DAYS"
+        daysAmount={30}
+        yieldPercent={1.55}
+        stakingHistory={datiledStakingFakeData}
+      />
+    </MainLayout>
+  )
 }
