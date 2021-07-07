7281ae3693e2bc091742918e8993d69b3550143c
DAO1 Team <developer@dao1.org>
2021-07-07 17:01:22 +0300

    Add connection logic on staking page

--- src/components/connectionStatus/connectionStatus.jsx
+++ src/components/connectionStatus/connectionStatus.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEthers, shortenAddress } from '@usedapp/core'
 
 import WalletIcon from '../../assets/wallet-red.png'
 import { Button } from '../'
@@ -9,15 +11,73 @@ import {
   accountAddress,
   connectButton,
 } from './connectionStatus.module.scss'
+import { connectionAction } from '../../actions/connectionAction'
+import MakeQuerablePromise from '../../utils/querable-promise'
 
 export function ConnectionStatus() {
+  const { account, error, deactivate, activateBrowserWallet } = useEthers()
+
+  const dispatch = useDispatch()
+  const isConnected = useSelector((state) => state.connectionReducer)
+
+  const activateWallet = async () => {
+    const activateBrowserWalletPromise = MakeQuerablePromise(
+      activateBrowserWallet(),
+    )
+    activateBrowserWalletPromise.then(
+      function() {
+        if (activateBrowserWalletPromise.isFulfilled()) {
+          dispatch(connectionAction(true))
+        }
+      },
+      function() {
+        /* code if some error */
+
+        dispatch(connectionAction(false))
+      },
+    )
+  }
+
+  const deactivateWallet = () => {
+    deactivate()
+    dispatch(connectionAction(false))
+  }
+
+  const ConnectionResult = () => {
+    if (error) {
+      return (
+        <Button onClick={activateWallet} className={connectButton}>
+          Error
+        </Button>
+      )
+    }
+
+    if (isConnected) {
+      return (
+        <>
+          <div className={accountAddressBlock}>
+            <img className={walletIcon} src={WalletIcon} alt="Wallet" />
+            <span className={accountAddress}>
+              {account && shortenAddress(account)}
+            </span>
+          </div>
+          <Button onClick={deactivateWallet} className={connectButton}>
+            Disconnect Wallet
+          </Button>
+        </>
+      )
+    }
+
+    return (
+      <Button onClick={activateWallet} className={connectButton}>
+        Connect Wallet
+      </Button>
+    )
+  }
+
   return (
     <div className={connectionStatus}>
-      <div className={accountAddressBlock}>
-        <img className={walletIcon} src={WalletIcon} alt="Wallet" />
-        <span className={accountAddress}>0xeFd3...bf3b</span>
-      </div>
-      <Button className={connectButton}>Connect Wallet</Button>
+      <ConnectionResult />
     </div>
   )
 }
--- src/pages/staking/staking.jsx
+++ src/pages/staking/staking.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useEthers } from '@usedapp/core'
 
-import { MainLayout, StakingCard } from '../../components'
+import {
+  connectMessage,
+  connectMessageInnerContainer,
+  connectMessageInnerContainerTitle,
+} from './staking.module.scss'
+import { MainLayout, StakingCard, Title, Button } from '../../components'
+import { connectionAction } from '../../actions/connectionAction'
+import MakeQuerablePromise from '../../utils/querable-promise'
 
 const datiledStakingFakeData = [
   {
@@ -60,36 +69,79 @@ const datiledStakingFakeData = [
 ]
 
 export const Staking = () => {
+  const { activateBrowserWallet } = useEthers()
+  const isConnected = useSelector((state) => state.connectionReducer)
+  const dispatch = useDispatch()
+
+  const activateWallet = async () => {
+    const activateBrowserWalletPromise = MakeQuerablePromise(
+      activateBrowserWallet(),
+    )
+    activateBrowserWalletPromise.then(
+      function() {
+        if (activateBrowserWalletPromise.isFulfilled()) {
+          dispatch(connectionAction(true))
+        }
+      },
+      function() {
+        /* code if some error */
+
+        dispatch(connectionAction(false))
+      },
+    )
+  }
+
+  const Content = () => {
+    if (isConnected) {
+      return (
+        <>
+          <div className="col">
+            <StakingCard
+              name="DAO1 — FIXED 30 DAYS"
+              daysAmount={30}
+              yieldPercent={1.55}
+              stakingHistory={datiledStakingFakeData}
+            />
+            <StakingCard
+              name="DAO1 — FIXED 60 DAYS"
+              daysAmount={60}
+              yieldPercent={4.5}
+              stakingHistory={[]}
+            />
+          </div>
+          <div className="col">
+            <StakingCard
+              name="DAO1 — FIXED 60 DAYS"
+              daysAmount={60}
+              yieldPercent={4.5}
+              stakingHistory={[]}
+            />
+            <StakingCard
+              name="DAO1 — FIXED 90 DAYS"
+              daysAmount={90}
+              yieldPercent={11.05}
+              stakingHistory={datiledStakingFakeData}
+            />
+          </div>
+        </>
+      )
+    }
+
+    return (
+      <div className={connectMessage}>
+        <div className={connectMessageInnerContainer}>
+          <Title className={connectMessageInnerContainerTitle} level={3}>
+            Please connect wallet
+          </Title>
+          <Button onClick={activateWallet}>Connect</Button>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <MainLayout>
-      <div className="col">
-        <StakingCard
-          name="DAO1 — FIXED 30 DAYS"
-          daysAmount={30}
-          yieldPercent={1.55}
-          stakingHistory={datiledStakingFakeData}
-        />
-        <StakingCard
-          name="DAO1 — FIXED 60 DAYS"
-          daysAmount={60}
-          yieldPercent={4.5}
-          stakingHistory={[]}
-        />
-      </div>
-      <div className="col">
-        <StakingCard
-          name="DAO1 — FIXED 60 DAYS"
-          daysAmount={60}
-          yieldPercent={4.5}
-          stakingHistory={[]}
-        />
-        <StakingCard
-          name="DAO1 — FIXED 90 DAYS"
-          daysAmount={90}
-          yieldPercent={11.05}
-          stakingHistory={datiledStakingFakeData}
-        />
-      </div>
+      <Content />
     </MainLayout>
   )
 }
