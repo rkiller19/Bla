d26fad1e5826df01fe345874bdca6bd2446f4d16
DAO1 Team <developer@dao1.org>
2021-07-12 10:58:01 +0300

    Add config for chainId 1337 and 137 DAO-47

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { DAppProvider } from '@usedapp/core'
+import { ChainId, DAppProvider, MULTICALL_ADDRESSES } from '@usedapp/core'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import './sass/index.scss'
-import {App} from './App'
+import { App } from './App'
 import { rootReducer } from './reducers'
 
 const store = createStore(
@@ -13,9 +13,20 @@ const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
 
+const config = {
+  readOnlyUrls: {
+    [ChainId.Localhost]: 'https://farm-staging.dao1.org/rpc-ganache',
+  },
+  multicallAddresses: {
+    [ChainId.Localhost]: 'https://farm-staging.dao1.org/rpc-ganache',
+    ...MULTICALL_ADDRESSES,
+  },
+  supportedChains: [ChainId.Mainnet, ChainId.Localhost, 137],
+}
+
 ReactDOM.render(
   <Provider store={store}>
-    <DAppProvider>
+    <DAppProvider config={config}>
       <App />
     </DAppProvider>
   </Provider>,
